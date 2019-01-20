77e89fc017a10842f5fc758e00ea24baf5aed2cd
Tony Ou <tony.ou@mail.mcgill.ca>
2019-01-19 21:00:04 -0500

    update

--- home.js
+++ home.js
@@ -1,8 +1,21 @@
-var $toTop = $('.scroll');
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100px) {
-        $toTop.fadeIn();
-    } else if ($toTop.is(':visible')) {
-        $toTop.fadeOut();
-    }
-});
+// var $toTop = $('.scroll');
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 100px) {
+//         $toTop.fadeIn();
+//     } else if ($toTop.is(':visible')) {
+//         $toTop.fadeOut();
+//     }
+// });
+
+// let content = document.getElementsByClassName('dropdownContent');
+//
+
+
+// $(document).ready(function(){
+//
+//   $('.button').click(function(){
+//     $(this).next().show();
+//   });
+//
+//
+// });
