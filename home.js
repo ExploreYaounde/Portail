0d77a9d677dfa990b61996f1bab06b83ff3e1230
Michael  Li <er.li@mail.mcgill.ca>
2019-01-20 05:19:04 -0500

    changes

--- home.js
+++ home.js
@@ -16,20 +16,7 @@
 //     $(".explore").toggle());
 //   });
 // });
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
 
 
 $(function() {
@@ -146,6 +133,7 @@ function initMap() {
       // icon:
     });
   }
+}
 //
 //   document.getElementById('submit').addEventListener('click', function() {
 //     calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -191,3 +179,19 @@ function initMap() {
 //   }
 //
 // }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  };
+}
+
